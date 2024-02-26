e52bd62ed15f3a41ff9771ff6fc1282a9eff80a7
MagistrBYTE <dementevds@gmail.com>
2024-02-26 15:32:02 +0500

    refactor: Доработка ViewModelFileSystem

--- Lotus.Core/Source/Mvvm/ViewModelFileSystem/LotusCollectionViewModelFileSystem.cs
+++ Lotus.Core/Source/Mvvm/ViewModelFileSystem/LotusCollectionViewModelFileSystem.cs
@@ -1,96 +1,67 @@
-﻿//=====================================================================================================================
-// Проект: Модуль базового ядра
-// Раздел: Подсистема ViewModel для файловой системы
-// Автор: MagistrBYTE aka DanielDem <dementevds@gmail.com>
-//---------------------------------------------------------------------------------------------------------------------
-/** \file LotusCollectionViewModelFileSystem.cs
-*		CollectionViewMode для элементов файловой системы.
-*/
-//---------------------------------------------------------------------------------------------------------------------
-// Версия: 1.0.0.0
-// Последнее изменение от 30.04.2023
-//=====================================================================================================================
 using System;
-//=====================================================================================================================
-namespace Lotus
+
+namespace Lotus.Core
 {
-	namespace Core
-	{
-		//-------------------------------------------------------------------------------------------------------------
-		/** \addtogroup CoreFileSystem
-		*@{*/
-		//-------------------------------------------------------------------------------------------------------------
-		/// <summary>
-		/// Коллекция для отображения элементов файловой системы
-		/// </summary>
-		//-------------------------------------------------------------------------------------------------------------
-		public class CollectionViewModelFileSystem : CollectionViewModelHierarchy<ILotusViewModelHierarchy,
-			ILotusFileSystemEntity>
-		{
-			#region ======================================= КОНСТРУКТОРЫ ==============================================
-			//---------------------------------------------------------------------------------------------------------
-			/// <summary>
-			/// Конструктор по умолчанию инициализирует объект класса предустановленными значениями
-			/// </summary>
-			//---------------------------------------------------------------------------------------------------------
-			public CollectionViewModelFileSystem()
-				: base(String.Empty)
-			{
+    /** \addtogroup CoreFileSystem
+	*@{*/
+    /// <summary>
+    /// Коллекция для отображения элементов файловой системы.
+    /// </summary>
+    public class CollectionViewModelFileSystem : CollectionViewModelHierarchy<ILotusViewModelHierarchy,
+        ILotusFileSystemEntity>
+    {
+        #region Constructors
+        /// <summary>
+        /// Конструктор по умолчанию инициализирует объект класса предустановленными значениями.
+        /// </summary>
+        public CollectionViewModelFileSystem()
+            : base(string.Empty)
+        {
 
-			}
+        }
 
-			//---------------------------------------------------------------------------------------------------------
-			/// <summary>
-			/// Конструктор инициализирует объект класса указанными параметрами
-			/// </summary>
-			/// <param name="name">Имя коллекции</param>
-			//---------------------------------------------------------------------------------------------------------
-			public CollectionViewModelFileSystem(String name)
-				: base(name)
-			{
-			}
+        /// <summary>
+        /// Конструктор инициализирует объект класса указанными параметрами.
+        /// </summary>
+        /// <param name="name">Имя коллекции.</param>
+        public CollectionViewModelFileSystem(string name)
+            : base(name)
+        {
+        }
 
-			//---------------------------------------------------------------------------------------------------------
-			/// <summary>
-			/// Конструктор инициализирует объект класса указанными параметрами
-			/// </summary>
-			/// <param name="name">Имя коллекции</param>
-			/// <param name="source">Источник данных</param>
-			//---------------------------------------------------------------------------------------------------------
-			public CollectionViewModelFileSystem(String name, ILotusFileSystemEntity source)
-				: base(name, source)
-			{
-			}
-			#endregion
+        /// <summary>
+        /// Конструктор инициализирует объект класса указанными параметрами.
+        /// </summary>
+        /// <param name="name">Имя коллекции.</param>
+        /// <param name="source">Источник данных.</param>
+        public CollectionViewModelFileSystem(string name, ILotusFileSystemEntity source)
+            : base(name, source)
+        {
+        }
+        #endregion
 
-			#region ======================================= МЕТОДЫ ILotusCollectionViewModelHierarchy =================
-			//---------------------------------------------------------------------------------------------------------
-			/// <summary>
-			/// Создание конкретной ViewModel для указанной модели
-			/// </summary>
-			/// <param name="model">Модель</param>
-			/// <param name="parent">Родительский элемент ViewModel</param>
-			/// <returns>ViewModel</returns>
-			//---------------------------------------------------------------------------------------------------------
-			public override ILotusViewModelHierarchy CreateViewModelHierarchy(System.Object model, ILotusViewModelHierarchy? parent)
-			{
-				if (model is CFileSystemFile file)
-				{
-					return new ViewModelFileSystemFile(file, parent);
-				}
-
-				if (model is CFileSystemDirectory directory)
-				{
-					return new ViewModelDirectorySystemFile(directory, parent);
-				}
-
-                throw new NotImplementedException("Model must be type <CFileSystemFile> or <CFileSystemDirectory>");
+        #region ILotusCollectionViewModelHierarchy methods
+        /// <summary>
+        /// Создание конкретной ViewModel для указанной модели.
+        /// </summary>
+        /// <param name="model">Модель.</param>
+        /// <param name="parent">Родительский элемент ViewModel.</param>
+        /// <returns>ViewModel.</returns>
+        public override ILotusViewModelHierarchy CreateViewModelHierarchy(object model, ILotusViewModelHierarchy? parent)
+        {
+            if (model is CFileSystemFile file)
+            {
+                return new ViewModelFileSystemFile(file, parent);
             }
-			#endregion
-		}
-		//-------------------------------------------------------------------------------------------------------------
-		/**@}*/
-		//-------------------------------------------------------------------------------------------------------------
-	}
+
+            if (model is CFileSystemDirectory directory)
+            {
+                return new ViewModelDirectorySystemFile(directory, parent);
+            }
+
+            throw new NotImplementedException("Model must be type <CFileSystemFile> or <CFileSystemDirectory>");
+        }
+        #endregion
+    }
+    /**@}*/
 }
-//=====================================================================================================================

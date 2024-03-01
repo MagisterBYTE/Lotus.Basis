c24dd9893e580580441f8f177aaa2f8dc2b1b4bd
MagistrBYTE <dementevds@gmail.com>
2024-03-01 21:10:25 +0500

    chore: Мелкие правки

--- Lotus.Core/Source/Tree/ITreeNode.cs
+++ Lotus.Core/Source/Tree/ITreeNode.cs
@@ -3,39 +3,39 @@ using System.Collections.Generic;
 
 namespace Lotus.Core
 {
-	/// <summary>
-	/// Определение интерфейса для объектов которые могут находиться в дереве.
-	/// </summary>
-	public interface ITreeNode : ILotusIdentifierId<Guid>, ILotusCheckOne<ITreeNode>
-	{
-		/// <summary>
-		/// Статус выбора узла.
-		/// </summary>
-		bool IsSelected { get; set; }
+    /// <summary>
+    /// Определение интерфейса для объектов которые могут находиться в дереве.
+    /// </summary>
+    public interface ITreeNode : ILotusIdentifierId<Guid>, ILotusCheckOne<ITreeNode>
+    {
+        /// <summary>
+        /// Статус выбора узла.
+        /// </summary>
+        bool IsSelected { get; set; }
 
-		/// <summary>
-		/// Статус выбора узла флажком.
-		/// </summary>
-		bool? IsChecked { get; set; }
+        /// <summary>
+        /// Статус выбора узла флажком.
+        /// </summary>
+        bool? IsChecked { get; set; }
 
-		/// <summary>
-		/// Статус раскрытия узла.
-		/// </summary>
-		bool IsExpanded { get; set; }
+        /// <summary>
+        /// Статус раскрытия узла.
+        /// </summary>
+        bool IsExpanded { get; set; }
 
-		/// <summary>
-		/// Порядок следования узла(индекс его расположения).
-		/// </summary>
-		int Order { get; set; }
+        /// <summary>
+        /// Порядок следования узла(индекс его расположения).
+        /// </summary>
+        int Order { get; set; }
 
-		/// <summary>
-		/// Родительский узел.
-		/// </summary>
-		ITreeNode? IParentTreeNode { get; set; }
+        /// <summary>
+        /// Родительский узел.
+        /// </summary>
+        ITreeNode? IParentTreeNode { get; set; }
 
-		/// <summary>
-		/// Список дочерних узлов.
-		/// </summary>
-		IEnumerable<ITreeNode>? IChildNodes { get; }
-	}
+        /// <summary>
+        /// Список дочерних узлов.
+        /// </summary>
+        IEnumerable<ITreeNode>? IChildNodes { get; }
+    }
 }
